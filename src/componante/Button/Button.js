51230edc9220fe48ea0d48b4642761f073b11244
ashwini <ashwini.swe@gmail.com>
2023-04-25 13:51:44 +0530

    solution

--- src/componante/Button/Button.js
+++
@@ -1,9 +0,0 @@
-import './Button.css';
-export function Button(){
-
-    return(
-    <div class="App-header">
-    <button>Get started</button>
-    </div>
-    )
-}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { Logo } from './componante/logo/logo';
-import { Benifits } from './componante/Benifits/Benifits';
-import { Button } from './componante/Button/Button';
+import App from './App';
+
+
 
  
 
@@ -13,9 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
 
-    <Logo />
-    <Benifits />
-    <Button />
+   <App />
     
    
   </React.StrictMode>
@@ -24,4 +20,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
